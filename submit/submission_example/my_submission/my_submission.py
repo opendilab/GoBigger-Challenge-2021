f2c1d82dc4b2b6959ef283c1354c13a5bef2780e
zhangming <zhangming@sensetime.com>
2021-10-11 12:28:46 +0800

    add basesubmission in sample

--- submit/submission_example/my_submission/my_submission.py
+++ submit/submission_example/my_submission/my_submission.py
@@ -1,6 +1,18 @@
 import random
 
-from gobigger.submit import BaseSubmission
+
+class BaseSubmission:
+
+    def __init__(self, team_name, player_names):
+        self.team_name = team_name
+        self.player_names = player_names
+
+    def get_actions(self, obs):
+        '''
+        Overview:
+            You must implement this function.
+        '''
+        raise NotImplementedError
 
 
 class MySubmission(BaseSubmission):
