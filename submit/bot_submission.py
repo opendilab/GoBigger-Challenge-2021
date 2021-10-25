526705fd569812b5b3d85a7ebcd187790350e277
zhangming <zhangming@sensetime.com>
2021-10-25 16:50:15 +0800

    fix BaseSubmission

--- submit/bot_submission.py
+++ submit/bot_submission.py
@@ -1,5 +1,18 @@
-from gobigger.submit import BaseSubmission
 from gobigger.agents import BotAgent
+
+
+class BaseSubmission:
+
+    def __init__(self, team_name, player_names):
+        self.team_name = team_name
+        self.player_names = player_names
+
+    def get_actions(self, obs):
+        '''
+        Overview:
+            You must implement this function.
+        '''
+        raise NotImplementedError
 
 
 class BotSubmission(BaseSubmission):
